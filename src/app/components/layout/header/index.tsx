f285b73f92944659dd1e6b1c3f2c773d3e7745f7
pearl <pearlodi7@gmail.com>
2025-12-06 09:36:13 -0800

    update

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -24,7 +24,7 @@ const Buttons = () => {
               >
                 {theme === "light" ? <Sun />: <MoonStar />}
               </button>
-            </div>
+            </div>n
           
     </div>
   
